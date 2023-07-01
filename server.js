21c2946138c0fdb89b86def440a1579b80fc325e
Yogesh Mundra <123637075+YogeshMun0304@users.noreply.github.com>
2023-07-01 16:52:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import path from "path";
 import {fileURLToPath} from "url";
 dotenv.config();
 connectDB(); 
-const __filename = fileURLToPath{import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
                                  
 const app = express()
