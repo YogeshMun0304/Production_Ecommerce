c17077ea4c23f8c5909561d4ce45d50e5feac997
Yogesh Mundra <123637075+YogeshMun0304@users.noreply.github.com>
2023-07-01 17:14:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname,'./client.build')))
+app.use(express.static(path.join(__dirname,'./client/build')))
 app.use("/api/v1/auth",authRoute)
 app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/product",ProductRoute)
